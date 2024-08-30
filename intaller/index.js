84d715b31817dd49a9da7bf327ac281e43c9df01
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2024-08-30 21:29:37 +0200

    feat: begin

---
+++ intaller/index.js
@@ -0,0 +1,18 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+function init_shell() {
+    console.clear()
+    console.log("   ▄████████  ▄██████▄     ▄████████")
+    console.log("  ███    ███ ███    ███   ███    ███")
+    console.log("  ███    ███ ███    ███   ███    █▀ ")
+    console.log("  ███    ███ ███    ███  ▄███▄▄▄    ")
+    console.log("▀███████████ ███    ███ ▀▀███▀▀▀    ")
+    console.log("  ███    ███ ███    ███   ███       ")
+    console.log("  ███    ███ ███    ███   ███       ")
+    console.log("  ███    █▀   ▀██████▀    ███       ")
+    console.log(" ")
+    console.log("All For One v1.0")
+}
+init_shell()
